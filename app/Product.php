f260213321911d113dc2e118a791898c32639c90
Valeria Granada Rodas <55161707+vale0722@users.noreply.github.com>
2019-12-12 20:54:18 -0500

    Update Product.php

--- app/Product.php
+++ app/Product.php
@@ -6,7 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    public function Invoices(){
-        return $this->belongsToMany(Invoce::class);
+    public function Invoices()
+    {
+        return $this->belongsToMany(Invoice::class);
     }
 }
