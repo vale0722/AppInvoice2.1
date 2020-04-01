530d88c6aa0b8b4462bcd6f20eb71ce3c7aa8dc0
Valeria Granada Rodas <55161707+vale0722@users.noreply.github.com>
2020-03-31 20:45:25 -0500

    create role

--- database/seeds/RolesSeeder.php
+++ database/seeds/RolesSeeder.php
@@ -75,7 +75,7 @@ class RolesSeeder extends Seeder
 
         $role = Role::create(['name' => 'client', 'description' => 'Cliente']);
 
-        //assinging invoice permissions to the client
+        //asinging invoice permissions to the client
         $role->givePermissionTo('invoices.view.associated');
         $role->givePermissionTo('invoices.show');
         $role->givePermissionTo('invoices.pay');
@@ -84,5 +84,17 @@ class RolesSeeder extends Seeder
         //asignig user permissions to the client
         $role->givePermissionTo('clients.update.associated');
         $role->givePermissionTo('users.your.show');
+
+        $role = Role::create(['name' => 'treasurer', 'description' => 'Tesorero']);
+
+        //asignig invoice permissions to the treasurer
+        $role->givePermissionTo('invoices.view');
+        $role->givePermissionTo('invoices.show');
+        $role->givePermissionTo('invoices.export');
+        $role->givePermissionTo('invoices.anuled');
+
+        //asignig client permissions to the treasurer
+        $role->givePermissionTo('clients.view');
+        $role->givePermissionTo('clients.show');
     }
 }
